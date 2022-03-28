45121d02d2a7199cff2a3da37d1b42b33876e04b
darkelle <djilani.cardineau@epitech.eu>
2022-03-28 23:16:40 +0200

    data

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -30,15 +30,10 @@ export class InfoComponent implements OnInit {
       ]
     },
     {
-      name: "Bourse",
+      name: "Travail",
       info: [
-        "Si tu pars de trois à douze mois dans le cadre du programme Erasmus, tu bénéficieras d’une allocation qui t’aidera à financer une partie de ton séjour. Tu peux t’attendre à recevoir entre 150 et 250 euros par mois pour un échange en Belgique. Cette allocation peut aller jusqu’à 400 euros par mois si tu effectues un stage inclut dans tes études.",
-        "Si tu pars hors Erasmus, sache que les communautés françaises et néerlandaises offrent des bourses sur critères sociaux aux citoyens européens. Renseigne-toi sur le site de la Fédération Wallonie-Bruxelles.",
-        "A noter, si vous êtes boursier sur critères sociaux, vous continuez de percevoir les versements de votre bourse."
+        "Pour travailler en Belgique, les étudiants français n’ont pas besoin d’accomplir une démarche particulière. Depuis janvier 2017, il est conseillé cependant de ne pas dépasser un contingent de 475 heures par année civile. Au-delà vous devrez payer des cotisations sociales comme tout salarié. Sous ce volume horaire, vous paierez uniquement une cotisation de solidarité (2 % maximum).",
       ],
-      link: [
-
-      ]
     }
   ]
   panelOpenState: boolean = false
