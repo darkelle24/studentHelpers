a02ee09b0207f2eb6ee8e9a555ebdcb08f8f4768
darkelle <djilani.cardineau@epitech.eu>
2022-05-03 12:15:16 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, isDevMode } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router, NavigationEnd } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { ApiService } from './core/_services/api.service';
 import { TopicsService } from './core/_services/topics.service';
 import { ShareComponent } from './page/share/share.component';
+import { AngularFireAnalytics } from '@angular/fire/compat/analytics';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +14,13 @@ import { ShareComponent } from './page/share/share.component';
 })
 export class AppComponent {
 
-  constructor(public topics: TopicsService) {
+  constructor(public topics: TopicsService, analytics: AngularFireAnalytics) {
     this.topics.actualizeTopics()
+    if (!isDevMode())
+      analytics.setAnalyticsCollectionEnabled(true)
+    else {
+      analytics.setAnalyticsCollectionEnabled(false)
+    }
   }
 
 }
